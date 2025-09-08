22a50b4428dfecdf5c66a170495992ec7e01744d
byeongmin <sa98077@naver.com>
2025-09-08 11:59:55 +0900

    민감정보 제외중

--- src/main/java/com/saladay/saladay_api/util/config/WebMvcConfig.java
+++ src/main/java/com/saladay/saladay_api/util/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.saladay.saladay_api.util.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+        @Value("${com.saladay.cors.url}")
+        private String url;
+
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**") // 모든 경로
-                    .allowedOrigins("http://localhost:5173") // 요청 허용할 주소
+                    .allowedOrigins(url) // 요청 허용할 주소
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowCredentials(true); // 쿠키 포함 시 true
         }
-    //@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true") 특정만 열고 싶을때는 컨트롤러에 이렇게 매핑.
 }
